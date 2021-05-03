d3d892a02524e7e533aac2ed3ed4e4868c700cea
mcoutolive <59898295+mcoutolive@users.noreply.github.com>
2021-05-03 16:40:42 -0300

    Create 04insertInto.sql

---
+++ 04insertInto.sql
@@ -0,0 +1,28 @@
+select * from alunos
+
+insert into alunos values ('Mateus Luiz', 'MogiGuacu')
+
+insert into alunos values ('José Wilson', 'MogiMirim')
+
+
+select * from disciplina
+
+insert into disciplina values ('SQL', 64)
+
+insert into disciplina values ('Algoritmos', 32)
+
+
+select * from professor
+
+insert into professor values ('José da Silva', 'MogiGuacu')
+
+insert into professor values ('Sandro José', 'EstivaGerbi')
+
+
+select * from historico
+
+insert into historico values (1,0,9.5,1000,1,1)
+
+insert into historico values (2,10,5.2,1001,2,2)
+
+insert into historico values (1,0,9.5,1000,2,2)
