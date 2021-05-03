f8f484d808136c1d7a262cddb2e06a8fe3090e75
mcoutolive <59898295+mcoutolive@users.noreply.github.com>
2021-05-03 16:16:20 -0300

    Create tablePrimarykey.sql

---
+++ tablePrimarykey.sql
@@ -0,0 +1,7 @@
+create table alunos (ra int not null identity (1000,1),
+
+                                  nome varchar (100),
+
+                                  cidade varchar (40),
+
+                                  constraint pk_ra primary key (ra))
